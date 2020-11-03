591db6f0c5aba4e6a83539bbe4c08807f35e5e83
Clément <duvivier.clement@gmail.com>
2020-11-03 15:54:30 +0100

    Fin Tp6

--- TP6/app/src/main/java/com/example/retrofit/GithubService.kt
+++ TP6/app/src/main/java/com/example/retrofit/GithubService.kt
@@ -1,5 +1,6 @@
 package com.example.retrofit
 
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -7,10 +8,10 @@ import retrofit2.http.Query
 
 interface GithubService {
     @GET("/users/{user}/repos")
-    fun listRepos(@Path("user") user: String?): List<Repo?>?
+    fun listRepos(@Path("user") user: String?): Call<List<Repo?>>?
 
     @GET("/search/repositories")
-    fun searchRepos(@Query("q") query: String?): List<Repo?>?
+    fun searchRepos(@Query("q") query: String?): Call<Repos>
 
     companion object {
         const val ENDPOINT = "https://api.github.com"
